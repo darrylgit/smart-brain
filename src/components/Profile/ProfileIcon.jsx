09ff3686ec1b66d4d7b59ea9696ae2c44cd0aced
darrylgit <darrylsmithup@gmail.com>
2020-10-11 13:56:32 -0400

    Add dropdown

--- src/components/Profile/ProfileIcon.jsx
+++ src/components/Profile/ProfileIcon.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
 class ProfileIcon extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       dropdownOpen: false
-    }
+    };
   }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  };
 
   render() {
     return (
-      <div class="pa4 tc lol">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          class="br-100 h3 w3 dib" alt="avatar" />
+      <div className='pa4 tc'>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag='span'
+            data-toggle='dropdown'
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img
+              src='http://tachyons.io/img/logo.jpg'
+              className='br-100 h3 w3 dib'
+              alt='avatar'
+            />
+          </DropdownToggle>
+          <DropdownMenu
+            className='b--transparent shadow-5'
+            style={{
+              backgroundColor: 'rgba(255, 255, 255, 0.5)'
+            }}
+          >
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem>Sign Out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
-    )
+    );
   }
 }
 
