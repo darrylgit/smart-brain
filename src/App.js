d3c3f46dabababaf413508f62f285c4cf22125fc
darrylgit <darrylsmithup@gmail.com>
2020-11-07 22:55:37 -0500

    Load user age and pet

--- src/App.js
+++ src/App.js
@@ -54,7 +54,9 @@ class App extends Component {
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
+        joined: data.joined,
+        pet: data.pet,
+        age: data.age
       }
     });
   };
