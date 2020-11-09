45d65ffde56e6402da5bde4f9904718703b670b8
darrylgit <darrylsmithup@gmail.com>
2020-11-08 22:48:39 -0500

    Create onFormChange handler

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,74 +1,107 @@
 import React from 'react';
 import './Profile.css';
 
-const Profile = ({ isProfileOpen, toggleModal, user }) => {
-  return (
-    <div className='profile-modal'>
-      <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white'>
-        <main className='pa4 black-80 w-80'>
-          <img
-            src='http://tachyons.io/img/logo.jpg'
-            className='br-100 h3 w3 dib'
-            alt='avatar'
-          />
-          <h1>{user.name}</h1>
-          <h4>{`Images Submitted: ${user.entries}`}</h4>
-          <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
-          <hr />
-          <label className='mt2 fw6' htmlFor='user-name'>
-            Name
-          </label>
-          <input
-            className='pa2 ba w-100'
-            placeholder={user.name}
-            type='text'
-            name='user-name'
-            id='name'
-            data-lpignore='true'
-          />
-          <label className='mt2 fw6' htmlFor='user-age'>
-            Age
-          </label>
-          <input
-            className='pa2 ba w-100'
-            placeholder={user.age}
-            type='text'
-            name='user-age'
-            id='age'
-            data-lpignore='true'
-          />
-          <label className='mt2 fw6' htmlFor='user-age'>
-            Pet
-          </label>
-          <input
-            className='pa2 ba w-100'
-            placeholder={user.pet}
-            type='text'
-            name='user-age'
-            id='age'
-            data-lpignore='true'
-          />
-          <div
-            className='mt4'
-            style={{ display: 'flex', justifyContent: 'space-evenly' }}
-          >
-            <button className='b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20'>
-              Save
-            </button>
-            <button
-              className='b pa2 grow pointer hover-white w-40 bg-red b--black-20'
-              onClick={toggleModal}
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.user.name,
+      age: this.props.user.age,
+      pet: this.props.user.pet
+    };
+  }
+
+  onFormChange = event => {
+    switch (event.target.name) {
+      case 'user-name':
+        this.setState({ name: event.target.value });
+        break;
+      case 'user-age':
+        this.setState({ age: event.target.value });
+        break;
+      case 'user-pet':
+        this.setState({ pet: event.target.value });
+        break;
+      default:
+        return;
+    }
+  };
+
+  render() {
+    const { user } = this.props;
+    return (
+      <div className='profile-modal'>
+        <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white'>
+          <main className='pa4 black-80 w-80'>
+            <img
+              src='http://tachyons.io/img/logo.jpg'
+              className='br-100 h3 w3 dib'
+              alt='avatar'
+            />
+            <h1>{this.state.name}</h1>
+            <h4>{`Images Submitted: ${user.entries}`}</h4>
+            <p>{`Member since: ${new Date(
+              user.joined
+            ).toLocaleDateString()}`}</p>
+            <hr />
+            <label className='mt2 fw6' htmlFor='user-name'>
+              Name
+            </label>
+            <input
+              onChange={this.onFormChange}
+              className='pa2 ba w-100'
+              placeholder={user.name}
+              type='text'
+              name='user-name'
+              id='name'
+              data-lpignore='true'
+            />
+            <label className='mt2 fw6' htmlFor='user-age'>
+              Age
+            </label>
+            <input
+              onChange={this.onFormChange}
+              className='pa2 ba w-100'
+              placeholder={user.age}
+              type='text'
+              name='user-age'
+              id='age'
+              data-lpignore='true'
+            />
+            <label className='mt2 fw6' htmlFor='user-age'>
+              Pet
+            </label>
+            <input
+              onChange={this.onFormChange}
+              className='pa2 ba w-100'
+              placeholder={user.pet}
+              type='text'
+              name='user-pet'
+              id='age'
+              data-lpignore='true'
+            />
+            <div
+              className='mt4'
+              style={{ display: 'flex', justifyContent: 'space-evenly' }}
             >
-              Cancel
-            </button>
+              <button className='b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20'>
+                Save
+              </button>
+              <button
+                className='b pa2 grow pointer hover-white w-40 bg-red b--black-20'
+                onClick={this.props.toggleModal}
+              >
+                Cancel
+              </button>
+            </div>
+          </main>
+          <div className='modal-close' onClick={this.props.toggleModal}>
+            &times;
           </div>
-        </main>
-        <div className='modal-close' onClick={toggleModal}>
-          &times;
-        </div>
-      </article>
-    </div>
-  );
-};
+        </article>
+      </div>
+    );
+  }
+}
 
 export default Profile;
