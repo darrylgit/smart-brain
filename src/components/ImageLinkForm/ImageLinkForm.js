3cd3865c333bc6eb407b7f39c4189fd41c54dfac
darrylgit <darrylsmithup@gmail.com>
2020-11-12 22:06:17 -0500

    Update profile with auth middleware

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,19 +5,26 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        {'This Magic Brain will detect faces in your pictures. Give it a try.'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <input
+            className='f4 pa2 w-70 center'
+            type='tex'
+            onChange={onInputChange}
+            data-lpignore
+          />
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
-          >Detect</button>
+          >
+            Detect
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default ImageLinkForm;
