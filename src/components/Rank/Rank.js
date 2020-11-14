45f01276042f9af1e41696469abf537f32652db8
darrylgit <darrylsmithup@gmail.com>
2020-11-14 14:47:27 -0500

    Show rank using Lambda function

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,44 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ''
+    };
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries === this.props.entries) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = entries => {
+    fetch(
+      `https://x7iz5mijgj.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`
+    )
+      .then(res => res.json())
+      .then(data => this.setState({ emoji: data.input }))
+      .catch(console.log);
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='white f3'>
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>{this.props.entries}</div>
+        <div className='white f3'>{`Rank Badge: ${this.state.emoji}`}</div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
